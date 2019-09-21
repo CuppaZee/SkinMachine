59c6b58ffe4ecba06acbef356ded84ade037f63a
sohcah <mail@sohcah.dev>
2019-09-21 16:53:36 +0100

    Improve Submit Page

--- src/pages/Submit.vue
+++ src/pages/Submit.vue
@@ -1,8 +1,8 @@
 <template>
-  <q-page :class="{'text-white':$store.state.DB.dark}">
+  <q-page padding :class="{'text-white':$store.state.DB.dark}">
     <div class="text-h4">Submit Skins</div>
     <p>
-      Please send your Skins, along with Titles, Descriptions, Your Username and any other information to <a href="mailto:mail@cuppazee.uk">mail@cuppazee.uk</a>.
+      Please send your Skins, along with Titles, Descriptions, Your Username and any other information to <a :class="{'text-blue-3':$store.state.DB.dark}" href="mailto:mail@cuppazee.uk">mail@cuppazee.uk</a>.
     </p>
     <p>
       We also accept other suggestions, such as for Template Skins and new features. You can send them to the same address.
